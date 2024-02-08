0b610a9bb33a904b658a6c557f2973227a18f420
bryandj <bryandjw6@gmail.com>
2024-02-07 23:36:54 -0600

    Animación en los labels del monto del cliente

--- src/main/java/com/jmc/appbanckjavafx/Controllers/Client/TransactionCellController.java
+++ src/main/java/com/jmc/appbanckjavafx/Controllers/Client/TransactionCellController.java
@@ -1,18 +1,15 @@
 package com.jmc.appbanckjavafx.Controllers.Client;
 
 import com.jmc.appbanckjavafx.Models.Transaction;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.animation.FadeTransition;
 import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionCellController implements Initializable {
-    public FontAwesomeIconView in_icon;
-    public FontAwesomeIconView out_icon;
     public Label trans_date_lbl;
     public Label sender_lbl;
     public Label receiver_lbl;
@@ -29,10 +26,14 @@ public class TransactionCellController implements Initializable {
         receiver_lbl.textProperty().bind(transaction.receiverProperty());
         trans_date_lbl.textProperty().bind(transaction.dateProperty().asString());
 
-        // Configuración de la animación de desvanecimiento
-        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(3), amount_lbl);
-        amount_lbl.setOpacity(0); // Establecer la opacidad inicial a 0
-        fadeTransition.setToValue(1); // Establecer la opacidad final a 1
-        fadeTransition.play(); // Reproducir la animación de desvanecimiento
+        // Configurar la animación de escala
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(1), amount_lbl);
+        scaleTransition.setFromX(1.0); // Escala inicial en X
+        scaleTransition.setFromY(1.0); // Escala inicial en Y
+        scaleTransition.setToX(1.5); // Escala final en X
+        scaleTransition.setToY(1.5); // Escala final en Y
+        scaleTransition.setAutoReverse(true); // Permitir reversión de la animación
+        scaleTransition.setCycleCount(ScaleTransition.INDEFINITE); // Repetir la animación indefinidamente
+        scaleTransition.play(); // Reproducir la animación de escala
     }
 }
